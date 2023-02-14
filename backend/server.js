1250071f9df97d1f7f4516e850b487a5273a12db
borimirdotjs <b.kitev@gmail.com>
2023-02-14 14:39:05 +0200

    REST API

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT  || 5000;
+const colors = require('colors');
+const connectDB = require('./config/db')
 
+connectDB();
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
-
 app.use('/api/goals', require('./routes/goalRoutes'))
-
 app.use(errorHandler)
 
 app.listen(port, ()=> {
